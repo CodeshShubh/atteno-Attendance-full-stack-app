4f5d437a9102c29084aa444099fa62ff238e5b50
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2024-06-22 22:36:08 +0530

    initial userPersent Page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,9 @@ import {ChakraProvider} from '@chakra-ui/react'
 import './App.css'
 import MLogin from './components/Login/MLogin';
 import MHome from './components/Home/MHome';
+import MUserProfile from './components/User/MUserProfile';
+import Temuser from './components/User/temuser';
+
 
 function App() {
 
@@ -15,7 +18,9 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<MHome/> }/>
-          <Route path='login' element={<MLogin/> }/>
+          <Route path='/login' element={<MLogin/> }/>
+          <Route path='/user' element={<MUserProfile/> }/>
+          <Route path='/temuser' element={<Temuser/> }/>
         </Routes>
       </Router>
       </ChakraProvider>
---
+++ frontend/src/components/User/MUserProfile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MUserProfile = () => {
+  return (
+    <div>MUserProfile</div>
+  )
+}
+
+export default MUserProfile
