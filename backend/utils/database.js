b03378b02ff6793047768f3b60705698bd00bebe
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2024-07-17 17:12:38 +0530

    update in backend

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async()=>{
 
-    await mongoose.connect("mongodb+srv://shubhanshusaadhiyaan:mack2010m@cluster0.ojpi4af.mongodb.net/" ,{
+    await mongoose.connect(process.env.MONGO_URI ,{
         dbName : "Atteno"
     })
     .then((c)=>console.log(`DB Connected to ${c.connection.host}`))
